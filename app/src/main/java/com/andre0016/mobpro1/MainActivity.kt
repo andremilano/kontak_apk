95655340700c8efccae4209ba998a8b2ab435d6d
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-05-06 16:40:11 +0700

    menghapus import tidak terpakai

--- app/src/main/java/com/andre0016/mobpro1/MainActivity.kt
+++ app/src/main/java/com/andre0016/mobpro1/MainActivity.kt
@@ -3,14 +3,27 @@ package com.andre0016.mobpro1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.andre0016.mobpro1.navigation.SetupNavGraph
-import com.andre0016.mobpro1.ui.theme.Mobpro1Theme
+import com.andre0016.mobpro1.util.SettingsDataStore
+import com.andre0016.mobpro1.util.getCustomColorScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val dataStore = SettingsDataStore(this)
         setContent {
-            Mobpro1Theme {
+            val themeColor by dataStore.themeColorFlow.collectAsState("Blue")
+            val isDarkTheme = isSystemInDarkTheme()
+
+            val colorScheme = getCustomColorScheme(themeColor, isDarkTheme)
+
+            MaterialTheme(
+                colorScheme = colorScheme
+            ) {
                 SetupNavGraph()
             }
         }
